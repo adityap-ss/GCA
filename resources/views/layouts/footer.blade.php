31ea39b574498676c3b0e145078e9994f6186635
adityapratap <aditya@gorilladesk.com>
2021-01-27 11:50:49 +0530

    add chnages

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,29 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <ul class="nav">
+            <li class="nav-item">
+                <a href="https://creative-tim.com" target="blank" class="nav-link">
+                    {{ __('') }}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="https://updivision.com" target="blank" class="nav-link">
+                    {{ __('') }}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    {{ __('') }}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    {{ __('') }}
+                </a>
+            </li>
+        </ul>
+        <div class="copyright">
+          
+        </div>
+    </div>
+</footer>
